2775e2827424d9f6c76633d343ab6ca3d00d3c91
RajivGope007 <rajivgope007@gmail.com>
2022-11-08 18:33:18 +0530

    First Commit

---
+++ SumEven.py
@@ -0,0 +1,6 @@
+numbers = [12, 75, 150, 180, 145, 525, 50]
+sum = 0
+for i in numbers:
+    if i % 2 == 0:
+        sum += i
+print(sum)
---
+++ Vote.py
@@ -0,0 +1,5 @@
+age = int(input("Enter Your Age:- "))
+if age >= 18 :
+    print("I can vote.")
+else :
+    print("I can't vote.")
